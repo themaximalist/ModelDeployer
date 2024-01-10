7924744e4cadff96194834650b76fcfb52b67e4a
Brad Jasper <hello@themaximalist.com>
2024-01-10 00:21:55 -0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,17 @@
 
 APP_NAME="modeldeployer.com"
 
+# Check if the first argument is "--deploy"
+if [[ $1 == "--deploy" ]]; then
+  FORCE_DEPLOY=true
+else
+  FORCE_DEPLOY=false
+fi
+
 UPDATES=$(git pull)
-if [[ $UPDATES != *"Already up to date"* ]]; then
+
+# Check if updates are found or if forced to deploy
+if [[ $UPDATES != *"Already up to date"* ]] || [[ $FORCE_DEPLOY == true ]]; then
   npm install
 
   if pm2 list | grep -q $APP_NAME; then
@@ -14,4 +23,5 @@ if [[ $UPDATES != *"Already up to date"* ]]; then
 else
   echo "No updates found. Nothing to do."
 fi
+
 
