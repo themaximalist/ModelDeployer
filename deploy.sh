e00ebd36957c2edbedf1f8a5f4d59242602b3769
Brad Jasper <hello@themaximalist.com>
2024-01-10 00:19:21 -0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+APP_NAME="modeldeployer.com"
+
+UPDATES=$(git pull)
+if [[ $UPDATES != *"Already up to date"* ]]; then
+  npm install
+
+  if pm2 list | grep -q $APP_NAME; then
+    pm2 restart $APP_NAME
+  else
+    pm2 start src/index.js --name $APP_NAME
+  fi
+else
+  echo "No updates found. Nothing to do."
+fi
+
