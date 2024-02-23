e333b5e685b94220870e3fe57eb91fd3aee9e742
Brad Jasper <hello@themaximalist.com>
2024-02-22 23:15:59 -0800

    tailwind

--- public/build.sh
+++ public/build.sh
@@ -4,6 +4,6 @@ NAME="modeldeployer"
 DOMAIN="modeldeployer.themaximalist.com"
 TITLE="Model Deployer — Deploy AI models in production"
 DESCRIPTION="Model Deployer lets you easily deploy, rate limit, and manage AI models in production"
-CLASSES="gap-md"
+CLASSES="gap-sm-2"
 
 readme-to-index "$NAME" "$DOMAIN" "$TITLE" "$DESCRIPTION" "$CLASSES"
